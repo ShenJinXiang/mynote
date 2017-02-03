b7889c422efb2d4a7328c17c0c899099f3987fdb
shenjinxiang <sjx-sword@163.com>
2017-02-03 15:34:22 +0800

    设置线条宽度颜色。。

--- mds/javascript/canvas/001.js
+++ mds/javascript/canvas/001.js
@@ -1,3 +1,21 @@
+	/*
+(function(){
+	let canvas = document.getElementById('mycanvas');
+	canvas.width = 800;
+	canvas.height = 800;
+	let context = canvas.getContext('2d');
+
+	// 开始绘制
+	context.moveTo(100, 350);
+	context.lineTo(500, 350);
+	context.lineTo(500, 200);
+	context.lineTo(700, 400);
+	context.lineTo(500, 600);
+	context.lineTo(500, 450);
+	context.lineTo(100, 450);
+	context.stroke();
+})();
+	*/
 (function(){
 	let canvas = document.getElementById('mycanvas');
 	canvas.width = 800;
@@ -7,5 +25,7 @@
 	// 开始绘制
 	context.moveTo(100, 100);
 	context.lineTo(700, 700);
+	context.lineWidth = 10;
+	context.strokeStyle = "#058";
 	context.stroke();
 })();
